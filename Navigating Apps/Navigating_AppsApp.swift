02ae75bc3af2d634695deddea76c7b8e13fdf1a4
mk812y <mk812y@gmail.com>
2023-09-12 20:30:01 +0200

    Initial Commit

---
+++ Navigating Apps/Navigating_AppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigating_AppsApp.swift
+//  Navigating Apps
+//
+//  Created by Михаил Куприянов on 12.9.23..
+//
+
+import SwiftUI
+
+@main
+struct Navigating_AppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
